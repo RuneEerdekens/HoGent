ea63720ceee0327f02f88a33e4764f7a70924d45
RuneEerdekens <76566331+RuneEerdekens@users.noreply.github.com>
2023-09-29 13:16:38 +0200

    H1-Oef11

--- OOSD1/H1/src/cui/Oef11.java
+++ OOSD1/H1/src/cui/Oef11.java
@@ -7,15 +7,16 @@ public class Oef11 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scan = new Scanner(System.in);
-		System.out.print("Geef een getal  (> 999 en <= 9999) in: ");
+		System.out.print("Geef een getal (> 999 en <= 9999) in: ");
 		int num = scan.nextInt();
 		
-		double duizenden = num/1000;
-		double honderden = num/100 - num%1000;
-		double tienden = num/10;
-		double eenheeden = num;
+		int duizenttal = (num - (num % 1000));
+		int hondertal = (num - (num % 100)) - duizenttal;
+		int tiental = (num - (num % 10)) - (duizenttal + hondertal);
+		int eenheid = num - (duizenttal + hondertal + tiental);
 		
-		System.out.printf("Het getal bestaat uit:%n%.f duizentallen%n%d hondertallen%n%d tientallen %n%d eenheden", duizenden, honderden, num, num);
+		
+		System.out.printf("Het getal bestaat uit:%n%d duizentallen%n%d hondertallen%n%d tientallen %n%d eenheden", duizenttal /1000, hondertal /100, tiental / 10, eenheid);
 	}
 
 }
