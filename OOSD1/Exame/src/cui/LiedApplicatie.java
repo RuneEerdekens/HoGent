4b61b950449cad19f7f3be852d8a66a117b30464
TheFakeThanos <EerdekensInc@gmail.com>
2023-11-09 19:41:07 +0100

    random commit

--- OOSD1/Exame/src/cui/LiedApplicatie.java
+++ OOSD1/Exame/src/cui/LiedApplicatie.java
@@ -18,15 +18,14 @@ public class LiedApplicatie {
 		for (int lijstNum = 0; lijstNum < aantal; lijstNum++) {
 			System.out.printf("%nGeef de details van de liedjes voor afspeellijst %d%n", lijstNum + 1);
 			String text = "niet dansbaar";
-			
+
 			for (int liedNum = 0; liedNum < 3; liedNum++) {
 				System.out.printf("Lied %d - duur (min): ", liedNum + 1);
 				Lied lied = new Lied(leesGetalIn(""));
 				System.out.printf("Lied %d - aantal bpm: ", liedNum + 1);
 				lied.setBeatsPerMinut((int) leesGetalIn(""));
-				System.out.printf("Je koos een %s nummer van %d bpm.%n%n", lied.geefTempo(),
-						lied.getBeatsPerMinut());
-				
+				System.out.printf("Je koos een %s nummer van %d bpm.%n%n", lied.geefTempo(), lied.getBeatsPerMinut());
+
 				som += lied.geefDuurinSeconden();
 				if (lied.getBeatsPerMinut() >= 90) {
 					text = "dansbaar";
@@ -37,8 +36,7 @@ public class LiedApplicatie {
 		}
 
 		System.out.printf("%s is %d seconde!%n",
-				aantal > 1 ? "De gemiddelde duur van je afspeellijsten" : "De duur van je afspeellijst",
-				som / aantal);
+				aantal > 1 ? "De gemiddelde duur van je afspeellijsten" : "De duur van je afspeellijst", som / aantal);
 	}
 
 	private int maakLijsten() {
